236b062c8c1a8d5b6fecefea134489b065d6483e
Leonardo Redin Pinto Figueiró <leonardo.redin@dbccompany.com.br>
2020-08-27 16:43:39 -0300

    added colors

---
+++ src/theme/colors.js
@@ -0,0 +1,62 @@
+const types = {
+  BUG: '#8CB230',
+  DARK: '#58575F',
+  DRAGON: '#0F6AC0',
+  ELETRIC: '#EED535',
+  FAIRY: '#ED6EC7',
+  FIGHTING: '#D04164',
+  FIRE: '#FD7D24',
+  FLYING: '#748FC9',
+  GHOST: '#556AAE',
+  GRASS: '#62B957',
+  GROUND: '#DD7748',
+  ICE: '#61CEC0',
+  NORMAL: '#9DA0AA',
+  POISON: '#A552CC',
+  PSYCHIC: '#EA5D60',
+  ROCK: '#BAAB82',
+  STEEL: '#417D9A',
+  WATER: '#4A90DA',
+}
+
+const backgrounds = {
+  BUG: '#8BD674',
+  DARK: '#6F6E78',
+  DRAGON: '#7383B9',
+  ELETRIC: '#F2CB55',
+  FAIRY: '#EBA8C3',
+  FIGHTING: '#EB4971',
+  FIRE: '#FFA756',
+  FLYING: '#83A2E3',
+  GHOST: '#8571BE',
+  GRASS: '#8BBE8A',
+  GROUND: '#F78551',
+  ICE: '#91D8DF',
+  NORMAL: '#B5B9C4',
+  POISON: '#9F6E97',
+  PSYCHIC: '#FF6568',
+  ROCK: '#D4C294',
+  STEEL: '#4C91B2',
+  WATER: '#58ABF6',
+}
+
+const heights = {
+  SHORT: '#FFC5E6',
+  NORMAL: '#AEBFD7',
+  TALL: '#AAACB8',
+}
+
+const weights = {
+  LIGHT: '#99CD7C',
+  NORMAL: '#57B2DC',
+  HEAVY: '#57B2DC',
+}
+
+const colors = {
+  types,
+  backgrounds,
+  heights,
+  weights,
+}
+
+export {colors}
