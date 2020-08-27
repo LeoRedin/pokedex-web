8610649b3d2bc9934fa81f6489e6e5a4e3788405
Leonardo Redin Pinto Figueiró <leonardo.redin@dbccompany.com.br>
2020-08-27 19:04:04 -0300

    basic info needed

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,12 +3,28 @@ import React from 'react'
 import {Wrapper, PokeInfo} from './styles'
 
 function Home() {
-  const pokes = [{name: 'Bulbassaur'}, {name: 'Charmander'}, {name: 'Squirtle'}]
+  const pokes = [
+    {number: '001', name: 'Bulbassaur', types: ['grass', 'poison']},
+    {number: '004', name: 'Charmander', types: ['fire']},
+    {number: '007', name: 'Squirtle', types: ['water']},
+  ]
 
   return (
     <Wrapper>
       {pokes.map(poke => (
-        <PokeInfo key={poke.name}> {poke.name}</PokeInfo>
+        <PokeInfo key={poke.name}>
+          #{poke.number}
+          <br />
+          {poke.name}
+          <br />
+          {poke.types.map(type => (
+            <>
+              <span key={type}>{type}</span> {''}
+            </>
+          ))}
+          <br />
+          imagem alinhada à direita
+        </PokeInfo>
       ))}
     </Wrapper>
   )
